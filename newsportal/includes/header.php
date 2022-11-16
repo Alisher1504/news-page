6ed52e0731ae59c6248ba125cab8064dc5b7b129
Alisher1504 <ruzmetov150499@gmail.com>
2022-11-16 15:50:12 +0300

    finish

---
+++ newsportal/includes/header.php
@@ -0,0 +1,43 @@
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary fixed-top">
+      <div class="container">
+        <a style="font-weight: bold; font-size: 35px;" class="navbar-brand" href="index.php"><i> <span style="color: yellow;">YOU</span>WEI-<span style="color: black;">BOX</span></i></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+              <a style="font-size: 20px;" class="nav-link text-white" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a style="font-size: 20px;" class="nav-link text-white" href="about-us.php">About</a>
+            </li>
+                 <li class="nav-item">
+              <a style="font-size: 20px;" class="nav-link text-white" href="index.php">News</a>
+            </li>
+             <li class="nav-item">
+              <a style="font-size: 20px;" class="nav-link text-white" href="contact-us.php">Contact us</a>
+            </li>
+            <li class="nav-item">
+              <a style="font-size: 20px;" class="nav-link text-white" href="admin/">Admin</a>
+            </li>
+  
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+      <div class="col-11">
+      <form name="search" action="search.php" method="post">
+              <div class="input-group">
+           
+              <input style="box-shadow: 0px 0px 10px 0px black;"  type="text" name="searchtitle" class="form-control" placeholder="Search for..." required>
+                <span class="input-group-btn">
+                  <button class="btn btn-secondary mx-1" type="submit">Go!</button>
+                </span>
+              </form>
+      </div>
+      </div>
+    </div>
